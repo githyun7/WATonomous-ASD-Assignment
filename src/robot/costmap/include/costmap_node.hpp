42a95259592ad191b2a2c2aaec5f05fa20d60d68
Hyunseo Lee <hsdaniel07@gmail.com>
2026-05-19 06:24:27 +0900

    Update

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,49 +1,41 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
- 
+
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
- 
+
 #include "costmap_core.hpp"
- 
+
 class CostmapNode : public rclcpp::Node {
   public:
-    CostmapNode(double resolution, int width, int height, int inflation_radius, int max_cost); 
- 
+    CostmapNode();
+
   private:
-    void lidarCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
+    // Runs every time a new /lidar message arrives
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
 
-    void initCostmap();
+    // Costmap helper functions
+    nav_msgs::msg::OccupancyGrid initializeCostmap();
+    bool convertToGrid(double x, double y, int & x_grid, int & y_grid);
+    void markFreeRay(nav_msgs::msg::OccupancyGrid & grid, double range, double angle);
+    void markObstacle(nav_msgs::msg::OccupancyGrid & grid, int x_grid, int y_grid);
+    void inflateObstacles(nav_msgs::msg::OccupancyGrid & grid);
 
-    void convert2Grid(double range, double angle, int& x_grid, int& y_grid);
-
-    void markObstacle(int x_grid, int y_grid);
-
-    double dist2Point(int x1, int y1, int x2, int y2);
-
-    double getCost(int x, int y, int new_x, int new_y);
-
-    void inflateObstacles();
-
-    void publishCostmap();
-
-    // costmap variables
     robot::CostmapCore costmap_;
-    std::vector<std::vector<int>> costmap_grid_;
 
-    // parameters
+    // ROS constructs
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
+
+    // Local costmap parameters
     double resolution_;
     int width_;
     int height_;
-    int inflation_radius_;
-    int max_cost_;
-
-    // publishers and subscribers and ros2 messages
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
-    nav_msgs::msg::OccupancyGrid costmap_msg_;
+    double origin_x_;
+    double origin_y_;
+    double inflation_radius_;
 };
- 
-#endif  
+
+#endif
