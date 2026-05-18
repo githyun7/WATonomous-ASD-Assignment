f445d44f3e4ebed9325ec0e6881d5b8390429286
Hyunseo Lee <hsdaniel07@gmail.com>
2026-05-19 06:20:05 +0900

    Update

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -2,38 +2,54 @@
 #define CONTROL_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+
 #include "nav_msgs/msg/path.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+
+#include "control_core.hpp"
+
 #include <optional>
 
-class PurePursuitController : public rclcpp::Node {
-public:
-    PurePursuitController();
+class ControlNode : public rclcpp::Node {
+  public:
+    ControlNode();
 
-private:
-    void control_loop();
+  private:
+    void pathCallback(const nav_msgs::msg::Path::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void controlLoop();
+
     std::optional<geometry_msgs::msg::PoseStamped> findLookaheadPoint();
-    geometry_msgs::msg::Twist computeVelocity(const geometry_msgs::msg::PoseStamped &target);
-    double computeDistance(const geometry_msgs::msg::Point &a, const geometry_msgs::msg::Point &b);
-    double extractYaw(const geometry_msgs::msg::Quaternion &quat);
+    geometry_msgs::msg::Twist computeVelocityCommand(const geometry_msgs::msg::PoseStamped & target);
 
-    // Subscribers and Publishers
+    double computeDistance(
+      const geometry_msgs::msg::Point & a,
+      const geometry_msgs::msg::Point & b
+    );
+
+    double extractYaw(const geometry_msgs::msg::Quaternion & q);
+    double normalizeAngle(double angle);
+    void publishStopCommand();
+
+    robot::ControlCore control_;
+
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
-
-    // Timer  
     rclcpp::TimerBase::SharedPtr control_timer_;
 
-    // Variables
     nav_msgs::msg::Path::SharedPtr current_path_;
-    nav_msgs::msg::Odometry::SharedPtr robot_odom_;
+    nav_msgs::msg::Odometry::SharedPtr current_odom_;
+
     double lookahead_distance_;
     double goal_tolerance_;
     double linear_speed_;
-    double angular_speed_;
+    double angular_gain_;
+    double max_angular_speed_;
 };
 
 #endif
